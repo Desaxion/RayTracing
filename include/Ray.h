5832e348d330c934a7a75119d7fd5f77e1f752d4
Anna Jonsson <annjo712@student.liu.se>
2022-09-19 12:01:40 +0200

    Cmake och börjat på ray

---
+++ include/Ray.h
@@ -0,0 +1,30 @@
+#pragma once
+
+class Ray {
+public:
+	// Default constructor
+	Ray() = default;
+
+	// Starting vertex, end vertex, direction vector
+	// Constructor
+	Ray(vec3 _start, vec3 _end, vec3 _dir);
+
+private:
+
+	// Pointer to the surface, on which the starting vertex is located
+	Surface* surface;
+
+	// Pointers to the previous and next ray
+	Ray* next;
+	Ray* prev;
+
+	// Ray color, use class ColorRGB
+
+
+	// Var
+	vec3 start;
+	vec3 end;
+
+	// direction från direction-klass?
+
+};
